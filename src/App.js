c8a2038955a1041950f9df0bd3603158fc1ce566
quangChe <quang.n.che@gmail.com>
2019-05-17 16:31:57 -0700

    Set up and tested basic routing functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+const PageOne = () => {
+  return (<div>Page One!</div>);
+}
+
+const PageTwo = () => {
+  return (
+  <div>
+    Page Two!
+    <button>
+      <Link to="/">Click Me</Link>
+    </button>
+  </div>
+  );
+}
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello World!</h1>
+      <div>
+        <Router>
+          <div>
+            <Route path="/" exact component={PageOne}/>
+            <Route path="/pagetwo" component={PageTwo}/>
+          </div>
+        </Router>
+      </div>
     )
   }
 }
