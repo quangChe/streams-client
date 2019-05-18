0d45db5ca189e224f6ecad793406027eb264c31c
quangChe <quang.n.che@gmail.com>
2019-05-18 00:47:01 -0700

    Wired up App.js with routes to Stream components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
-
-const PageOne = () => {
-  return (<div>Page One!</div>);
-}
-
-const PageTwo = () => {
-  return (
-  <div>
-    Page Two!
-    <button>
-      <Link to="/">Click Me</Link>
-    </button>
-  </div>
-  );
-}
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import StreamCreate from './components/streams/StreamCreate';
+import StreamDelete from './components/streams/StreamDelete';
+import StreamEdit from './components/streams/StreamEdit';
+import StreamList from './components/streams/StreamList';
+import StreamShow from './components/streams/StreamShow';
 
 class App extends React.Component {
   render() {
@@ -22,8 +12,11 @@ class App extends React.Component {
       <div>
         <Router>
           <div>
-            <Route path="/" exact component={PageOne}/>
-            <Route path="/pagetwo" component={PageTwo}/>
+            <Route path="/" exact component={StreamList}/>
+            <Route path="/streams/create" exact component={StreamCreate}/>
+            <Route path="/streams/edit" exact component={StreamEdit}/>
+            <Route path="/streams/delete" exact component={StreamDelete}/>
+            <Route path="/streams/show" exact component={StreamShow}/>
           </div>
         </Router>
       </div>
